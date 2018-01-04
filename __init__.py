41ee29ed26da7ce385c8b85cde5181d75e3b0ce4
Raimon Esteve <raimon@nan-tic.com>
2018-01-04 17:49:28 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# This file is part project_company module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import Pool
+
+def register():
+    pass
